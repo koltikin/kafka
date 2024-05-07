9c40c4aacf568b3dd49a62fc9c62c4507a41a54b
Alim <koltikin210@gmail.com>
2024-05-07 10:48:20 +0300

    checkout service added

--- checkout/src/main/java/com/cydeo/checkout/service/ProducerService.java
+++ checkout/src/main/java/com/cydeo/checkout/service/ProducerService.java
@@ -3,19 +3,16 @@ package com.cydeo.checkout.service;
 import com.cydeo.checkout.dto.CheckoutDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ProducerService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
     private final ObjectMapper objectMapper;
-
-    public ProducerService(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
-        this.kafkaTemplate = kafkaTemplate;
-        this.objectMapper = objectMapper;
-    }
 
     public String sendMessage(CheckoutDTO checkoutDTO) throws JsonProcessingException {
         String stringMessage = objectMapper.writeValueAsString(checkoutDTO);
